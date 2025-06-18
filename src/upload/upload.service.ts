2f96e26cd81b1ac386e8aa32d8f24f671d92989e
Trí Nguyễn <tri.nguyen@devsamurai.com>
2025-06-18 08:52:30 +0700

    Save chunked files to the storage

--- src/upload/upload.service.ts
+++ src/upload/upload.service.ts
@@ -46,6 +46,9 @@ export class UploadService {
         'video_to_uploaded',
       );
 
+      // Move this to comsumer
+      this.uploadToS3(files);
+
       return {
         message: 'Files uploaded successfully',
         files: [],
